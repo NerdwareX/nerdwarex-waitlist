ef1d99fc97d048f1d0891c857723f9772324b33a
William M. Tsikata <tsikatawill@gmail.com>
2023-01-30 09:38:32 +0000

    implement toast container

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import "aos/dist/aos.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <ToastContainer closeOnClick={false} hideProgressBar />
     <App />
   </React.StrictMode>
 );
-
