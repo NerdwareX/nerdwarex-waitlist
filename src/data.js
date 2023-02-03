a10798adebc924a860c9c50c2ea4ceadc283c0a4
Ashraf <104894013+ashrafolarinoye@users.noreply.github.com>
2023-02-03 08:09:55 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -86,7 +86,7 @@ export const courses = [
     heartIcon: "ri-heart-3-fill",
   },
   {
-    image: require("./assets/img.png"),
+    image: require("./assets/forex.png"),
     title: "Development",
     starIcon: "ri-star-s-fill",
     rating: "4.8",
