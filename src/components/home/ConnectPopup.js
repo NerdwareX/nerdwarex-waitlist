eddde0508c006f89201abb706db5f4b44bcdd20b
ashrafolarinoye <ashrafolarinoye@gmail.com>
2023-02-24 14:53:10 +0100

    wallet connect & unstoppable domain icon

--- src/components/home/ConnectPopup.js
+++ src/components/home/ConnectPopup.js
@@ -32,27 +32,27 @@ function ConnectPopup() {
         <Modal.Header closeButton>
           <Modal.Title className="text-[#2B2B8B] font-semibold text-[36px] text-left mb-6">Connect Wallet</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <motion.button
-             className="px-[23px] py-2 btn-primary rounded-[20px] text-white block mx-auto font-medium"
-              onClick={handleShow}
-              /*onClick={window['Connect']}
-              id={'connect'}*/
-              whileTap={{ scale: 0.9 }}
-            ><img src={Walletconnect} alt="wallet connect icon"
-              className="md:flex w-[10%] h-[10%] lg:[10%] block mx-auto font-medium"/>
-                WalletConnect
-          </motion.button>
-            <br/>
-          <motion.button
+          <Modal.Body>
+            <motion.button
               className="px-[23px] py-2 btn-primary rounded-[20px] text-white block mx-auto font-medium"
-              onClick={handleShow}
-              whileTap={{ scale: 0.9 }}
-            ><img src={Unstoppabledomain} alt="Unstoppable Domain" 
-              className="md:flex w-[5%] h-[5%] lg:[5%] block mx-auto font-medium"/>
-                Unstoppable Domain
-          </motion.button>
-        </Modal.Body>
+                onClick={handleShow}
+                /*onClick={window['Connect']}
+                id={'connect'}*/
+                whileTap={{ scale: 0.9 }}
+              ><img src={Walletconnect} alt="wallet connect icon"
+                className="md:flex w-[10%] h-[10%] lg:[10%] block mx-auto font-medium"/>
+                  WalletConnect
+            </motion.button>
+              <br/>
+            <motion.button
+                className="px-[23px] py-2 btn-primary rounded-[20px] text-white block mx-auto font-medium"
+                onClick={handleShow}
+                whileTap={{ scale: 0.9 }}
+              ><img src={Unstoppabledomain} alt="Unstoppable Domain" 
+                className="md:flex w-[5%] h-[5%] lg:[5%] block mx-auto font-medium"/>
+                  Unstoppable Domain
+            </motion.button>
+          </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
